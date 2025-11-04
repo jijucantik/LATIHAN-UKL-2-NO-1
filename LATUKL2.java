a00485cb2e44571ef31720f63c37e42f89aa9581
jijucantik <aziisekolah18@gmail.com>
2025-11-04 07:29:14 +0700

    Add files via upload

---
+++ LATUKL2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LATUKL2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int n = input.nextInt();
+
+        int hasil = 1;
+        for (int i = 1; i <= n; i++) {
+            hasil *= i;
+        }
+
+        System.out.println("Faktorial dari " + n + " adalah " + hasil);
+    }
+}
